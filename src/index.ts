c798707d6c56845e401224cd6aa4838e2ac03d59
William Grosset <williamhgrosset@gmail.com>
2024-03-14 22:40:03 -0600

    Add commander for CLI tooling

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 import chalk from 'chalk'
+import { Command } from 'commander'
 
 console.log(chalk.blue('Hello world'))
+
+const program = new Command()
+
+program.description('CLI tool to publish packages').version('0.0.0')
+
+program.parse()
