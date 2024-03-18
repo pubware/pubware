3be192b7bf226ce8bcf9a50dfe880ab088371138
William Grosset <williamhgrosset@gmail.com>
2024-03-18 12:16:21 -0600

    Add name to CLI tool and combine with term styling

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import chalk from 'chalk'
 import { Command } from 'commander'
 
-console.log(chalk.blue('Hello world'))
-
 const program = new Command()
 
-program.description('CLI tool to publish packages').version('0.0.0')
+program
+  .name('packpub')
+  .description(chalk.blue('CLI tool to publish packages'))
+  .version(chalk.yellow('0.0.0'))
 
 program.parse()
