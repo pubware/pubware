7a24f252b52bdbd2786a783653ecf44061099faa
William Grosset <williamhgrosset@gmail.com>
2024-03-28 16:01:20 -0600

    Add async file read method

---
+++ src/lib/read.ts
@@ -0,0 +1,18 @@
+import fs from 'node:fs/promises'
+
+interface Options {
+  encoding: BufferEncoding
+}
+
+const optsDefault: Options = {
+  encoding: 'utf8'
+}
+
+export async function fsRead(path: string, opts: Options = optsDefault) {
+  try {
+    const data = await fs.readFile(path, opts)
+    console.log(data)
+  } catch (err) {
+    console.error(err)
+  }
+}
