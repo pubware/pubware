5ea2452c55602781c409c3f79b98ad4734d06949
William Grosset <williamhgrosset@gmail.com>
2024-04-10 14:25:48 -0600

    Add log and prompt functionality to Plugin

--- src/plugins/plugin.ts
+++ src/plugins/plugin.ts
@@ -1,3 +1,5 @@
+import chalk from 'chalk'
+import { confirm, input } from '@inquirer/prompts'
 import FileSystem from '../core/fs/index.js'
 import HTTP from '../core/http/index.js'
 import Shell from '../core/shell/index.js'
@@ -25,6 +27,22 @@ class Plugin {
 
   set disabled(disabled: boolean) {
     this._disabled = disabled
+  }
+
+  log(message: string) {
+    console.log(chalk.green(message))
+  }
+
+  warn(message: string) {
+    console.log(chalk.yellow(message))
+  }
+
+  async promptInput(message: string): Promise<string> {
+    return await input({ message })
+  }
+
+  async promptConfirm(message: string): Promise<boolean> {
+    return await confirm({ message })
   }
 
   async read(path: string): Promise<string> {
