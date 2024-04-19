e13a905a5bb3234a6d5fc44f1f39750e3dcbf24b
William Grosset <williamhgrosset@gmail.com>
2024-04-19 14:46:19 -0600

    Update CLI logging and update example lifecycle hook

--- src/lib/cli.ts
+++ src/lib/cli.ts
@@ -1,6 +1,7 @@
 import { Command } from 'commander'
 import chalk from 'chalk'
 import Lifecycle from './lifecycle.js'
+import NPM from '../plugins/npm.js'
 
 class CLI {
   private static NAME = 'packpub'
@@ -18,12 +19,12 @@ class CLI {
   }
 
   /**
-   * Log info message to console.
+   * Log message to console.
    *
    * @param {string} message The message to log.
    */
   log(message: string) {
-    console.log(chalk.blue(message))
+    console.log(chalk.black(`[packpub]: ${message}`))
   }
 
   /**
@@ -36,14 +37,14 @@ class CLI {
 
     const lifecycle = new Lifecycle()
 
-    lifecycle
-      .on('pre-bump', () => console.log('Preparing to bump version!'))
-      .on('pre-commit', () => console.log('Preparing to git commit and push!'))
-      .on('post-publish', () => console.log('Just published!'))
+    // TODO Load plugins dynamically (await import(plugin))
+    const npm = new NPM()
 
-    await lifecycle.start()
+    lifecycle.on('bump', () => npm.bump())
 
-    this.log('CLI completed.')
+    await lifecycle.run()
+
+    this.log('CLI finished.')
   }
 }
 
