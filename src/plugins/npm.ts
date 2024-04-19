9aeb66951b1a69569bd9aa48f6cbc33d72d42d19
William Grosset <williamhgrosset@gmail.com>
2024-04-19 14:36:49 -0600

    Update npm plugin with error handling and new plugin API

--- src/plugins/npm.ts
+++ src/plugins/npm.ts
@@ -1,4 +1,4 @@
-import semver, { ReleaseType } from 'semver'
+import semver from 'semver'
 import Plugin from './plugin.js'
 
 class NPM extends Plugin {
@@ -18,39 +18,60 @@ class NPM extends Plugin {
     super('npm')
   }
 
-  async version(): Promise<string> {
-    const data = await this.read(NPM.FILES.PACKAGE_JSON_PATH)
-    const packageJson = JSON.parse(data)
-    return packageJson.version
-  }
-
-  async bump(release: ReleaseType): Promise<string> {
-    const data = await this.read(NPM.FILES.PACKAGE_JSON_PATH)
-    const packageJson = JSON.parse(data)
-    const version = semver.inc(packageJson.version, release)
-
-    packageJson.version = version
-    await this.write(
-      NPM.FILES.PACKAGE_JSON_PATH,
-      JSON.stringify(packageJson, null, 2) + '\n'
-    )
-
-    return version ?? ''
-  }
-
-  async build() {
+  private async version(): Promise<string> {
     try {
-      await this.exec(NPM.CMDS.BUILD)
+      const data = await this.read(NPM.FILES.PACKAGE_JSON_PATH)
+      const packageJson = JSON.parse(data)
+      return packageJson.version
     } catch (err) {
-      console.error(err)
+      throw err
     }
   }
 
-  async publish() {
+  private async build(): Promise<void> {
+    try {
+      await this.exec(NPM.CMDS.BUILD)
+    } catch (err) {
+      throw err
+    }
+  }
+
+  async beforeBump(): Promise<void> {
+    const version = await this.version()
+    this.output(`Package version: ${version}`)
+
+    this.output(`Running build...`)
+    await this.build()
+  }
+
+  async bump(): Promise<void> {
+    const response = await this.prompt('Which version?')
+
+    try {
+      const data = await this.read(NPM.FILES.PACKAGE_JSON_PATH)
+      const packageJson = JSON.parse(data)
+      const version = semver.inc(packageJson.version, 'patch')
+
+      if (!version) {
+        throw new Error('Semver failed to bump package version')
+      }
+
+      packageJson.version = version
+
+      await this.write(
+        NPM.FILES.PACKAGE_JSON_PATH,
+        JSON.stringify(packageJson, null, 2) + '\n'
+      )
+    } catch (err) {
+      throw err
+    }
+  }
+
+  async publish(): Promise<void> {
     try {
       await this.exec(NPM.CMDS.PUBLISH, NPM.FLAGS.DRY_RUN)
     } catch (err) {
-      console.error(err)
+      throw err
     }
   }
 }
