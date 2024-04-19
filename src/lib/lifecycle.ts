31566dfcebe8fcb96b5d1daafa824ac842cfeff4
William Grosset <williamhgrosset@gmail.com>
2024-04-19 14:45:35 -0600

    Update lifecycle events and decouple from plugins

--- src/lib/lifecycle.ts
+++ src/lib/lifecycle.ts
@@ -1,29 +1,23 @@
 import Queue from './queue.js'
-import NPM from '../plugins/npm.js'
-import Git from '../plugins/git.js'
 
-type Event = 'pre-bump' | 'pre-commit' | 'post-publish'
+type Event = 'beforeBump' | 'bump' | 'beforePublish' | 'publish' | 'postPublish'
 type Callback<T extends any[], R> = (...args: T) => Promise<R> | R
-
-interface Plugins {
-  npm: NPM
-  git: Git
-}
+type Hooks = Record<Event, Queue<Callback<any[], any>>>
 
 class Lifecycle {
-  private hooks: Record<Event, Queue<Callback<any[], any>>>
-  private plugins: Plugins
+  static EVENTS: Event[] = [
+    'beforeBump',
+    'bump',
+    'beforePublish',
+    'publish',
+    'postPublish'
+  ]
+  private hooks: Hooks = {} as Hooks
 
   constructor() {
-    this.hooks = {
-      'pre-bump': new Queue<Callback<any[], any>>(),
-      'pre-commit': new Queue<Callback<any[], any>>(),
-      'post-publish': new Queue<Callback<any[], any>>()
-    }
-    this.plugins = {
-      npm: new NPM(),
-      git: new Git()
-    }
+    Lifecycle.EVENTS.forEach(EVENT => {
+      this.hooks[EVENT] = new Queue<Callback<any[], any>>()
+    })
   }
 
   on<T extends any[], R>(event: Event, cb: Callback<T, R>): this {
@@ -35,23 +29,18 @@ class Lifecycle {
     const callbacks = this.hooks[event]
 
     while (!callbacks.isEmpty()) {
-      const cb = callbacks.remove()
+      const callback = callbacks.remove()
 
-      if (cb) {
-        await cb()
+      if (callback) {
+        await callback()
       }
     }
   }
 
-  async start(): Promise<void> {
-    await this.trigger('pre-bump')
-    // await this.plugins.npm.bump('patch')
-
-    await this.trigger('pre-commit')
-    // await this.plugins.git.commit()
-
-    // await this.plugins.npm.publish()
-    await this.trigger('post-publish')
+  async run(): Promise<void> {
+    for (const event of Lifecycle.EVENTS) {
+      await this.trigger(event)
+    }
   }
 }
 
