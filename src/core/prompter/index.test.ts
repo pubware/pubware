8a0eea95031e304a062e44a2b892b4d4aa33490a
William Grosset <williamhgrosset@gmail.com>
2024-05-02 18:13:40 -0600

    Add Prompter tests

---
+++ src/core/prompter/index.test.ts
@@ -0,0 +1,64 @@
+import { input, confirm, select } from '@inquirer/prompts'
+import Prompter from './index.js'
+
+jest.mock('@inquirer/prompts')
+
+describe('Prompter', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  test('input calls with correct message', async () => {
+    const message = "What's your name?"
+    const mockResponse = 'John Doe'
+    jest.mocked(input).mockResolvedValue(mockResponse)
+
+    const prompter = new Prompter()
+    const response = await prompter.input(message)
+
+    expect(input).toHaveBeenCalledWith({ message })
+    expect(response).toBe(mockResponse)
+  })
+
+  test('throws error when input fails', async () => {
+    expect(async () => {
+      const message = "What's your name?"
+      const error = new Error('Input failed')
+      jest.mocked(input).mockRejectedValue(error)
+
+      const prompter = new Prompter()
+      await prompter.input(message)
+    }).rejects.toThrow('Input failed')
+  })
+
+  test('confirm calls with correct message', async () => {
+    const message = 'Are you sure?'
+    const mockResponse = true
+    jest.mocked(confirm).mockResolvedValue(mockResponse)
+
+    const prompter = new Prompter()
+    const response = await prompter.confirm(message)
+
+    expect(confirm).toHaveBeenCalledWith({ message })
+    expect(response).toBe(mockResponse)
+  })
+
+  test('select calls with correct message and choices', async () => {
+    const message = 'Select an option'
+    const choices = [
+      { name: 'Option 1', value: '1' },
+      { name: 'Option 2', value: '2' }
+    ]
+    const mockResponse = '1'
+    jest.mocked(select).mockResolvedValue(mockResponse)
+
+    const prompter = new Prompter()
+    const response = await prompter.select(message, choices)
+
+    expect(select).toHaveBeenCalledWith({
+      message,
+      choices
+    })
+    expect(response).toBe(mockResponse)
+  })
+})
