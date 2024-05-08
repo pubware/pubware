2c221d7837b65449c1926d254d0eea167f32d016
William Grosset <williamhgrosset@gmail.com>
2024-05-08 09:46:07 -0600

    Add CLI integration tests

---
+++ src/lib/cli/index.integration.test.ts
@@ -0,0 +1,56 @@
+import { jest } from '@jest/globals'
+import CLI from './index.js'
+
+jest.mock('../logger/index.js')
+jest.mock('../config/index.js')
+jest.mock('../lifecycle/index.js')
+
+const Logger = (await import('../logger/index.js')).default
+const Config = (await import('../config/index.js')).default
+const Lifecycle = (await import('../lifecycle/index.js')).default
+
+describe('CLI', () => {
+  beforeAll(() => {
+    jest.spyOn(Logger.prototype, 'log').mockImplementation(() => {})
+    jest.spyOn(Lifecycle.prototype, 'run').mockResolvedValue()
+  })
+
+  afterAll(() => {
+    jest.restoreAllMocks()
+  })
+
+  test('initializes config with flags', async () => {
+    const initSpy = jest.spyOn(Config.prototype, 'init').mockResolvedValue()
+    const cli = new CLI()
+    await cli.run(['node', '../bin/packpub.js', '--dry-run', '--headless'])
+
+    expect(initSpy).toHaveBeenCalledWith({ dryRun: true, headless: true })
+
+    initSpy.mockRestore()
+  })
+
+  test('attaches events to lifecycle', async () => {
+    const lifecycleRunSpy = jest
+      .spyOn(Lifecycle.prototype, 'run')
+      .mockResolvedValue()
+    const lifecycleListenerSpy = jest.spyOn(Lifecycle.prototype, 'on')
+    const cli = new CLI()
+    await cli.run(['node', '../bin/packpub.js'])
+
+    expect(lifecycleListenerSpy).toHaveBeenCalled()
+
+    lifecycleRunSpy.mockRestore()
+  })
+
+  test('runs lifecycle', async () => {
+    const lifecycleSpy = jest
+      .spyOn(Lifecycle.prototype, 'run')
+      .mockResolvedValue()
+    const cli = new CLI()
+    await cli.run(['node', '../bin/packpub.js'])
+
+    expect(lifecycleSpy).toHaveBeenCalled()
+
+    lifecycleSpy.mockRestore()
+  })
+})
