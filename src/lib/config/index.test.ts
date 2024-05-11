708ac2a89dab4a6c314adde27397f9941db9e4bc
William Grosset <williamhgrosset@gmail.com>
2024-05-10 21:38:15 -0600

    Add tests for disabling internal plugins

--- src/lib/config/index.test.ts
+++ src/lib/config/index.test.ts
@@ -2,7 +2,7 @@ import { jest } from '@jest/globals'
 
 jest.unstable_mockModule('node:fs/promises', () => ({
   default: {
-    readFile: jest.fn().mockImplementation(() => JSON.stringify({}))
+    readFile: jest.fn()
   }
 }))
 
@@ -23,11 +23,77 @@ describe('Config', () => {
   })
 
   test('loads npm and git plugins', async () => {
+    jest.mocked(fs.readFile).mockResolvedValue(JSON.stringify({}))
     const config = new Config()
     await config.init({})
 
     expect(config.plugins.length).toBe(2)
     expect(config.plugins[0].constructor.name).toBe('NPM')
     expect(config.plugins[1].constructor.name).toBe('Git')
+  })
+
+  test('disables internal npm plugin', async () => {
+    jest.mocked(fs.readFile).mockResolvedValue(
+      JSON.stringify({
+        packpub: {
+          plugins: {
+            internal: {
+              npm: {
+                disabled: true
+              }
+            }
+          }
+        }
+      })
+    )
+    const config = new Config()
+    await config.init({})
+
+    expect(config.plugins.length).toBe(1)
+    expect(config.plugins[0].constructor.name).toBe('Git')
+  })
+
+  test('disables internal git plugin', async () => {
+    jest.mocked(fs.readFile).mockResolvedValue(
+      JSON.stringify({
+        packpub: {
+          plugins: {
+            internal: {
+              git: {
+                disabled: true
+              }
+            }
+          }
+        }
+      })
+    )
+    const config = new Config()
+    await config.init({})
+
+    expect(config.plugins.length).toBe(1)
+    expect(config.plugins[0].constructor.name).toBe('NPM')
+  })
+
+  test('disables both internal plugins', async () => {
+    jest.mocked(fs.readFile).mockResolvedValue(
+      JSON.stringify({
+        packpub: {
+          plugins: {
+            internal: {
+              npm: {
+                disabled: true
+              },
+              git: {
+                disabled: true
+              }
+            }
+          }
+        }
+      })
+    )
+    const config = new Config()
+    await config.init({})
+
+    expect(config.plugins.length).toBe(0)
   })
 })
