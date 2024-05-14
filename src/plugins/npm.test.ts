85ca02d9fac52cd6ae78018abb1b56ac84b16f70
William Grosset <williamhgrosset@gmail.com>
2024-05-14 16:19:40 -0600

    Add config tests for `npm` plugin

--- src/plugins/npm.test.ts
+++ src/plugins/npm.test.ts
@@ -12,11 +12,26 @@ describe('NPM', () => {
       .spyOn(npm, 'read')
       .mockResolvedValue(JSON.stringify({ version: '9.9.9' }))
     const execSpy = jest.spyOn(npm, 'exec').mockResolvedValue()
-    const logSpy = jest.spyOn(npm, 'log').mockImplementation(() => {})
+    const logSpy = jest.spyOn(npm, 'log')
     await npm.preBump()
 
     expect(execSpy).toHaveBeenCalledWith('npm run build')
     expect(logSpy).toHaveBeenCalledWith('Package version: 9.9.9')
+  })
+
+  test('supports custom build cmd', async () => {
+    const npm = new NPM({
+      config: {
+        buildCmd: 'npm run custom-build'
+      }
+    })
+    jest
+      .spyOn(npm, 'read')
+      .mockResolvedValue(JSON.stringify({ version: '9.9.9' }))
+    const execSpy = jest.spyOn(npm, 'exec').mockResolvedValue()
+    await npm.preBump()
+
+    expect(execSpy).toHaveBeenCalledWith('npm run custom-build')
   })
 
   test('`bump` runs npm version cmd', async () => {
@@ -30,12 +45,93 @@ describe('NPM', () => {
     )
   })
 
+  test('throws error for invalid prompt choice', async () => {
+    const npm = new NPM({})
+    jest
+      .spyOn(npm, 'promptSelect')
+      .mockResolvedValue('definitely-not-a-valid-version')
+    jest.spyOn(npm, 'exec').mockResolvedValue()
+
+    await expect(npm.bump()).rejects.toThrow(
+      'Must select a valid release type for bump'
+    )
+  })
+
+  test('supports pre-release choices with custom pre-release id', async () => {
+    const expectedSubset = [
+      {
+        name: 'prepatch',
+        value: 'prepatch',
+        description: 'Prepatch'
+      },
+      {
+        name: 'preminor',
+        value: 'preminor',
+        description: 'Preminor'
+      },
+      {
+        name: 'premajor',
+        value: 'premajor',
+        description: 'Premajor'
+      },
+      {
+        name: 'prerelease',
+        value: 'prerelease',
+        description: 'Prerelease'
+      }
+    ]
+    const npm = new NPM({
+      config: {
+        preReleaseId: 'alpha'
+      }
+    })
+    const promptSpy = jest.spyOn(npm, 'promptSelect').mockResolvedValue('patch')
+    jest.spyOn(npm, 'exec').mockResolvedValue()
+    await npm.bump()
+
+    expect(promptSpy).toHaveBeenCalledWith(
+      expect.any(String),
+      expect.arrayContaining(expectedSubset),
+      ''
+    )
+  })
+
+  test('supports custom version args', async () => {
+    const npm = new NPM({
+      config: {
+        versionArgs: '--custom-arg=foo'
+      }
+    })
+    jest.spyOn(npm, 'promptSelect').mockResolvedValue('patch')
+    const execSpy = jest.spyOn(npm, 'exec').mockResolvedValue()
+    await npm.bump()
+
+    expect(execSpy).toHaveBeenCalledWith(
+      'npm version patch --custom-arg=foo --git-tag-version=false'
+    )
+  })
+
+  test('supports tag commit arg', async () => {
+    const npm = new NPM({
+      config: {
+        tagCommit: true
+      }
+    })
+    jest.spyOn(npm, 'promptSelect').mockResolvedValue('patch')
+    const execSpy = jest.spyOn(npm, 'exec').mockResolvedValue()
+    await npm.bump()
+
+    expect(execSpy).toHaveBeenCalledWith(
+      'npm version patch  --git-tag-version=true'
+    )
+  })
+
   test('`prePublish` logs version', async () => {
     const npm = new NPM({})
     jest
       .spyOn(npm, 'read')
       .mockResolvedValue(JSON.stringify({ version: '1.1.1' }))
-    const logSpy = jest.spyOn(npm, 'log').mockImplementation(() => {})
+    const logSpy = jest.spyOn(npm, 'log')
     await npm.prePublish()
 
     expect(logSpy).toHaveBeenCalledWith('Package version: 1.1.1')
@@ -47,5 +143,17 @@ describe('NPM', () => {
     await npm.publish()
 
     expect(execSpy).toHaveBeenCalledWith('npm publish ')
+  })
+
+  test('supports custom publish args', async () => {
+    const npm = new NPM({
+      config: {
+        publishArgs: '--custom-arg=foo'
+      }
+    })
+    const execSpy = jest.spyOn(npm, 'exec').mockResolvedValue()
+    await npm.publish()
+
+    expect(execSpy).toHaveBeenCalledWith('npm publish --custom-arg=foo')
   })
 })
